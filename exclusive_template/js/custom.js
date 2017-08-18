832c8229dfe278a6fa540da09edf0e5b463601a3
softrashel <softrashel@gmail.com>
2017-08-18 16:21:50 +0600

    This is my fully responsive and exclusive template.

---
+++ exclusive_template/js/custom.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function() {
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    //Click event to scroll to top
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+    $('.tootlip').tooltip();
+    $("ul#ticker01").liScroll();
+});
+
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+});
+wow.init();
+
+jQuery(window).load(function() { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(100).css({
+        'overflow': 'visible'
+    });
+})
